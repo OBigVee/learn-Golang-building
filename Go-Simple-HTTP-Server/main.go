d67eb209a0d12fdf47e19767a29290cb24907173
OBigVee <adedejivictor98@gmail.com>
2023-02-25 21:15:26 +0100

    :construction:

--- Go-Simple-HTTP-Server/main.go
+++ Go-Simple-HTTP-Server/main.go
@@ -1,6 +1,14 @@
 package main
 import "fmt"
 
-func main(){
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
 
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
